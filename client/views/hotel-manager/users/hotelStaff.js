07a1f37b3e9341da9a4acb5afd4fd303d1fc3da7
Patrick Scott <patrick.lee.scott@gmail.com>
2014-07-09 16:57:35 -0400

    style users list

--- client/views/hotel-manager/users/hotelStaff.js
+++ client/views/hotel-manager/users/hotelStaff.js
@@ -7,5 +7,16 @@ Template.hotelStaff.helpers({
 Template.hotelStaffItem.helpers({
   emailAddress: function() {
     return this.emails[0].address;
+  },
+  role: function() {
+    if (Roles.userIsInRole(Meteor.userId(), ['hotel-staff'])) {
+      if (Roles.userIsInRole(Meteor.userId(), ['hotel-manager'])) {
+        return 'Manager'
+      } else {
+        return 'Staff'
+      }
+    } else {
+      return 'Invalid'
+    }
   }
 });
