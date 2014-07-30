1bf135f083bf751d70004f3c376b0459e4c08a87
Patrick Scott <patrick.lee.scott@gmail.com>
2014-07-30 01:23:58 -0400

    menu DOM

--- client/views/hotel-services/configure-services/room-service/menu/editMenuCategory.js
+++ client/views/hotel-services/configure-services/room-service/menu/editMenuCategory.js
@@ -20,6 +20,24 @@ Template.editMenuCategory.helpers({
   },
   menuCategoryDescriptionSchema: function() {
     return Schema.menuCategoryDescriptionSchema;
+  },
+  isChecked: function() {
+    // sets property 'checked' of input checkbox to 'checked' or ''
+    // if not configured, return ''
+    return this.active ? 'checked' : '';
+  }
+});
+
+
+Template.editMenuCategory.events({
+  'change #category-switch': function (e, tmpl) {
+    if (tmpl.$(e.currentTarget).prop('checked')) {
+      console.log('on');
+      Meteor.call('activateMenuCategory', this._id);
+    } else {
+      console.log('off');
+      Meteor.call('deactivateMenuCategory', this._id);
+    }
   }
 });
 
