3014ad509c7d12bcda671c7d8c59cc78e18b1b8a
Patrick Scott <patrick.lee.scott@gmail.com>
2014-08-12 17:35:25 -0400

    room service details

--- client/views/patron-orders/order-types/request-details/requestDetails.js
+++ client/views/patron-orders/order-types/request-details/requestDetails.js
@@ -1,17 +1,28 @@
 Template.requestDetails.helpers({
   when: function() {
-    return moment(this.request.options.date).calendar();
+    var options = this.request.options || {};
+    if (options && options.date) {
+      return moment(this.request.options.date).calendar();
+    } else {
+      return 'ASAP';
+    }
   },
   formattedDate: function() {
-    return moment(this.request.options.date).format('MMMM Do, YYYY h:mm a')
+    var options = this.request.options || {};
+    if (options && options.date) {
+      return moment(this.request.options.date).format('MMMM Do, YYYY h:mm a');
+    } else {
+      return "As Soon As Possible"
+    }
   },
   requestTemplate: function() {
     switch (this.request.type) {
       case 'transportation': 
-        return 'transportationRequestDetails'
+        return 'transportationRequestDetails';
+      case 'roomService': 
+        return 'roomServiceRequestDetails';
       default:
         return '';
-        break;
     }
   }
 });
