ead0fd050e0d7e3cc56de7c064835db9e12d5982
Patrick Scott <patrick.lee.scott@gmail.com>
2014-09-10 15:57:05 -0400

    show orders in reverse chronological order

--- client/views/patron-orders/patron-orders.js
+++ client/views/patron-orders/patron-orders.js
@@ -3,6 +3,6 @@ Template.patronOrders.helpers({
     return Orders.find({for: 'hotel', open: true}).count() > 0;
   },
   orders: function() {
-    return Orders.find({for: 'hotel', open: true});
+    return Orders.find({for: 'hotel', open: true}, {sort: {requestedAt: -1}});
   }
 });
