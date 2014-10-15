12e46ae986bed0928f001fbfae5288a6ad1f4bc3
Patrick Scott <patrick.lee.scott@gmail.com>
2014-10-15 18:31:20 -0400

    update customize background to accept hotelId from client which is either the users hotel Id or the session hotelid

--- collections/hotels.js
+++ collections/hotels.js
@@ -22,23 +22,24 @@ Hotels.allow({
 });
 
 Meteor.methods({
-  changeHotelPhoto: function(InkBlob) {
+  changeHotelPhoto: function(InkBlob, hotelId) {
     check(InkBlob, Object);
     var user = Meteor.user();
     
     if (user && Roles.userIsInRole(user, ['hotel-manager', 'admin'])) {
-      var hotelId = Meteor.user().hotelId;
+      
       if (hotelId) {
-        var hotel = Hotels.findOne();
+        var hotel = Hotels.findOne(hotelId);
+
         if (!hotel) {
-          Errors.throw('No hotel found for user');
+          Errors.throw('No hotel found');
         }
 
         Hotels.upsert(hotelId, {$set: {
           photoUrl: InkBlob.url,
           photoName: InkBlob.filename,
           photoSize: InkBlob.size
-        }}, {validate: false});  
+        }});  
       }
     } else {
       Errors.throw('You do not have proper access to this functionality.');
