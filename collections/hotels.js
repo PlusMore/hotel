fd189c8fbc3b1693b4a94787137cc4b24f318992
bfturner <brendanfturner@gmail.com>
2015-01-09 20:12:35 -0600

    edit hotel-info form with description support

--- collections/hotels.js
+++ collections/hotels.js
@@ -44,5 +44,19 @@ Meteor.methods({
     } else {
       Errors.throw('You do not have proper access to this functionality.');
     }
+  },
+  updateHotelInfo: function(doc) {
+    check(doc, Object);
+    var user = Meteor.user();
+    
+    if (user && Roles.userIsInRole(user, ['hotel-manager', 'admin'])) {
+      Hotels.upsert(doc.hotelId, {$set: {
+        name: doc.name,
+        phone: doc.phone,
+        description: doc.description
+      }});
+    } else {
+      Errors.throw('You do not have proper access to this functionality.');
+    }
   }
 });
