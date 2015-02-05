f94d7aeadcf4729f33310e7ab869b8dd1a26f0df
Patrick Scott <patrick.lee.scott@gmail.com>
2015-02-04 23:18:51 -0500

    config tips per service

--- client/views/hotel-services/configure-services/bell-service/configureBellService.js
+++ client/views/hotel-services/configure-services/bell-service/configureBellService.js
@@ -31,8 +31,8 @@ Template.configureBellService.helpers({
       return this.configuration.active ? 'checked' : '';
     }
   },
-  configureServiceAvailabilitySchema: function() {
-    return Schema.configureServiceAvailability;
+  configureServiceSchema: function() {
+    return Schema.configureService;
   }
 });
 
