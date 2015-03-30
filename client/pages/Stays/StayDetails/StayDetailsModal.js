96f1db963672a5e4a7e1d1bbbb9ac89e9805e7f6
bfturner <brendanfturner@gmail.com>
2015-03-30 14:21:51 -0500

    added ability to edit checkout date of currently active stays

--- client/pages/Stays/StayDetails/StayDetailsModal.js
+++ client/pages/Stays/StayDetails/StayDetailsModal.js
@@ -4,6 +4,15 @@ Template.StayDetailsModal.helpers({
   },
   users: function() {
     return Template.instance().users();
+  },
+  changeCheckoutSchema: function() {
+    return Schema.ChangeCheckoutDate;
+  },
+  pickadateOptions: function() {
+    return {
+      // can not change checkout date before todays date or checkInDate
+      min: Math.max(this.checkInDate, new Date())
+    };
   }
 });
 
@@ -21,3 +30,49 @@ Template.StayDetailsModal.created = function() {
     }
   }
 };
+
+Template.StayDetailsModal.rendered = function() {
+  this.$('.timepicker').pickatime({
+    onSet: function(selection) {
+      var minutes = selection.select;
+      var controlName = this.$node.attr('name');
+      var $form = this.$node.closest('form');
+      if (controlName === 'checkoutTime') {
+        $form.find('[name=checkoutMinutes]').val(minutes);
+      }
+    }
+  });
+};
+
+AutoForm.hooks({
+  changeCheckoutDate: {
+    before: {
+      changeCheckoutDate: function(doc, template) {
+        //return doc; (synchronous)
+        //return false; (synchronous, cancel)
+        //this.result(doc); (asynchronous)
+        //this.result(false); (asynchronous, cancel)
+        var stay = Stays.findOne(doc._id);
+        var checkoutDate = moment(doc.checkoutDate).zone(stay.zone).format('dddd, MM/DD');
+        if (confirm("Change the checkout date to " + checkoutDate + " at " + doc.checkoutTime + "?")) {
+          return doc;
+        }
+        return false;
+      }
+    },
+    // Called when any operation succeeds, where operation will be
+    // "insert", "update", "submit", or the method name.
+    onSuccess: function(operation, result, template) {
+      Messages.success('Successfully changed checkout date!');
+      BootstrapModalPrompt.dismiss();
+    },
+
+    // Called when any operation fails, where operation will be
+    // "validation", "insert", "update", "submit", or the method name.
+    onError: function(operation, error, template) {
+      if (operation !== "validation") {
+        Messages.error(error.message);
+      }
+    },
+  }
+});
