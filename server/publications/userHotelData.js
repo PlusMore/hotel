03ff8da2e3098780c4940814f239b330598c3d69
bfturner <brendanfturner@gmail.com>
2015-04-27 11:46:22 -0500

    split publications and store in own folder

---
+++ server/publications/userHotelData.js
@@ -0,0 +1,33 @@
+Meteor.publish('userHotelData', function(hotelId) {
+  var userId = this.userId;
+
+  if (userId) {
+    var fields = {
+        hotelId: 1,
+        emails: 1,
+        profile: 1,
+      },
+      user = Meteor.users.findOne({
+        _id: userId
+      }),
+      hotelId = user && user.hotelId || hotelId;
+    if (hotelId) {
+      return [
+        Meteor.users.find({
+          _id: userId
+        }, {
+          fields: fields
+        }),
+        Hotels.find({
+          _id: hotelId
+        })
+      ];
+    } else {
+      this.ready();
+      return null;
+    }
+  } else {
+    this.ready();
+    return null;
+  }
+});
