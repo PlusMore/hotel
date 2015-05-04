d83571642962f0d2a2bd8adc6fd3111ffd4401c3
bfturner <brendanfturner@gmail.com>
2015-05-04 13:03:21 -0500

    add kadira to browser-policy whitelist

--- server/browser-policy.js
+++ server/browser-policy.js
@@ -4,12 +4,18 @@ Meteor.startup(function() {
   BrowserPolicy.content.allowOriginForAll("http://*.filepicker.io");
   // Fonts & Icons
   BrowserPolicy.content.allowOriginForAll("https://*.gstatic.com");
+  BrowserPolicy.content.allowOriginForAll("http://*.gstatic.com");
   BrowserPolicy.content.allowOriginForAll("https://*.googleapis.com");
   BrowserPolicy.content.allowOriginForAll("http://*.googleapis.com");
+  BrowserPolicy.content.allowOriginForAll("https://*.google.com");
+  BrowserPolicy.content.allowOriginForAll("http://*.google.com");
   BrowserPolicy.content.allowOriginForAll("https://fontastic.s3.amazonaws.com");
   BrowserPolicy.content.allowOriginForAll("http://fontastic.s3.amazonaws.com");
   BrowserPolicy.content.allowOriginForAll("https://*.cloudfront.net");
   // PlusMore Apps
   BrowserPolicy.content.allowOriginForAll("http://*.plusmoretablets.com");
   BrowserPolicy.content.allowOriginForAll("https://*.plusmoretablets.com");
+  // misc
+  BrowserPolicy.content.allowOriginForAll("https://*.kadira.io");
+  BrowserPolicy.content.allowOriginForAll("http://*.kadira.io");
 });
