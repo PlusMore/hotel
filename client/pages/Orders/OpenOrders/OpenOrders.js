58302b46355595dca929a229ab5eada73e30bc15
bfturner <brendanfturner@gmail.com>
2015-05-06 12:34:23 -0500

    filter orders datatable based on group assignments

--- client/pages/Orders/OpenOrders/OpenOrders.js
+++ client/pages/Orders/OpenOrders/OpenOrders.js
@@ -2,11 +2,33 @@ Template.OpenOrders.helpers({
   selector: function() {
     var user = Meteor.user();
     var hotelId = Session.get('hotelId') || user.hotelId;
-    return {
-      hotelId: hotelId,
-      open: true,
-      handledBy: 'hotel'
-    };
+    if (Roles.userIsInRole(user._id, ['admin','hotel-manager'])) {
+      return {
+        hotelId: hotelId,
+        open: true,
+        handledBy: 'hotel',
+      }
+    } else {
+      var userGroupsCursor = user.memberOfGroups();
+      var userGroups = userGroupsCursor.fetch();
+      var userGroupsServices = [];
+      _.each(userGroups, function(group) {
+        _.each(group.servicesHandled, function(serviceType) {
+          userGroupsServices.push(serviceType);
+        })
+      })
+      return {
+        hotelId: hotelId,
+        open: true,
+        handledBy: 'hotel',
+        service: {
+          $exists: true
+        },
+        "service.type": {
+          $in: userGroupsServices
+        }
+      }
+    }
   }
 });
 
--- client/pages/Orders/OrderHistory/OrderHistory.js
+++ client/pages/Orders/OrderHistory/OrderHistory.js
@@ -2,11 +2,33 @@ Template.OrderHistory.helpers({
   selector: function() {
     var user = Meteor.user();
     var hotelId = Session.get('hotelId') || user.hotelId;
-    return {
-      hotelId: hotelId,
-      open: false,
-      handledBy: 'hotel'
-    };
+    if (Roles.userIsInRole(user._id, ['admin','hotel-manager'])) {
+      return {
+        hotelId: hotelId,
+        open: false,
+        handledBy: 'hotel'
+      }
+    } else {
+      var userGroupsCursor = user.memberOfGroups();
+      var userGroups = userGroupsCursor.fetch();
+      var userGroupsServices = [];
+      _.each(userGroups, function(group) {
+        _.each(group.servicesHandled, function(serviceType) {
+          userGroupsServices.push(serviceType);
+        })
+      })
+      return {
+        hotelId: hotelId,
+        open: false,
+        handledBy: 'hotel',
+        service: {
+          $exists: true
+        },
+        "service.type": {
+          $in: userGroupsServices
+        }
+      }
+    }
   }
 });
 
