74c9114c01afd3dc0fa983fbd5438c6d47218d05
bfturner <brendanfturner@gmail.com>
2015-05-08 13:43:11 -0500

    small clean-up; make group filter menu own template

--- client/pages/Orders/OpenOrders/OpenOrders.js
+++ client/pages/Orders/OpenOrders/OpenOrders.js
@@ -49,38 +49,6 @@ Template.OpenOrders.helpers({
     }
 
     return selector;
-  },
-  filterGroupSelect: function() {
-    var filterGroupId = Session.get('ordersDatatableFilterGroupId');
-    if (filterGroupId) {
-      return Groups.findOne(filterGroupId).name;
-    } else {
-      return "All";
-    }
-  },
-  allGroups: function() {
-    return Groups.find({
-      hotelId: Session.get('hotelId')
-    }, {
-      $sort: {
-        name: 1
-      }
-    });
-  }
-});
-
-Template.OpenOrders.events({
-  'change #order-group-filter': function(e, tmpl) {
-    e.preventDefault();
-    if (tmpl.$(e.currentTarget).val() != "all" && tmpl.$(e.currentTarget).val() != "none") {
-      Session.set('ordersDatatableFilterGroupId', tmpl.$(e.currentTarget).val());
-      var groupName = tmpl.$(e.currentTarget).find('option:selected').text();
-      Messages.success("Filter Applied: " + groupName);
-    } else if (tmpl.$(e.currentTarget).val() == "all") {
-      Session.set('ordersDatatableFilterGroupId', undefined);
-      Messages.success("Filter Applied: All");
-    }
-    forceReRender();
   }
 });
 
@@ -90,10 +58,6 @@ Template.OpenOrders.onCreated(function() {
     var hotelId = Session.get('hotelId');
     self.subscribe('groups', hotelId);
     // this prevents weirdness due to tabular's nonreactive selector
-    forceReRender();
-  });
-  self.autorun(function() {
-    var groupFilterId = Session.get('ordersDatatableFilterGroupId');
     forceReRender();
   });
 });
