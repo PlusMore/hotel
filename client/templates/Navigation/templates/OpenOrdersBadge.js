90f76953ac070b71a93a697f762e0285009829c0
bfturner <brendanfturner@gmail.com>
2015-05-12 15:15:52 -0500

    fix merge conflict

--- client/templates/Navigation/templates/OpenOrdersBadge.js
+++ client/templates/Navigation/templates/OpenOrdersBadge.js
@@ -1,7 +1,30 @@
 Template.OpenOrdersBadge.onCreated(function () {
   var self = this;
+
   self.autorun(function() {
-    var hotelId = Session.get('hotelId');
-    self.subscribe('openOrdersWidget', hotelId);
+    var user = Meteor.user();
+    var hotelId = Session.get('hotelId') || user.hotelId;
+
+    var selector = {
+      hotelId: hotelId,
+      open: true,
+      handledBy: 'hotel'
+    };
+
+    if (Roles.userIsInRole(user, ['hotel-manager', 'admin'])) {
+      // do not ammend selector if manager/admin
+    } else {
+      // select orders of groups user is in
+      var groups = user.memberOfGroups().fetch();
+      var handledServices = [];
+      _.each(groups, function(group) {
+        handledServices.push.apply(group.handledServices);
+      });
+      selector["service.type"] = {
+        $in: handledServices
+      };
+    }
+
+    self.subscribe('openOrdersWidget', selector);
   });
 });
