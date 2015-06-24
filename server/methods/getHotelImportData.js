24b2e04f304e7e7b5be196d4d2f08b254515129d
bfturner <brendanfturner@gmail.com>
2015-06-24 16:08:05 -0500

    make method to retrieve hotel-specific import details

---
+++ server/methods/getHotelImportData.js
@@ -0,0 +1,19 @@
+Meteor.methods({
+  getHotelImportData: function(fromAddress) {
+    check(fromAddress, String);
+
+    var hotel = Hotels.findOne({importsEmail: fromAddress});
+
+    if (!hotel) {
+      throw new Meteor.Error(500, "No hotel has importsEmail: " + fromAddress);
+    }
+
+    var importData = {
+      hotelId: hotel._id,
+      arrivalMinutes: hotel.arrivalMinutes(),
+      departureMinutes: hotel.departureMinutes()
+    };
+
+    return importData;
+  }
+});
